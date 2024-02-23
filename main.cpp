2eebda15cd4635d6675828f68a4605d705df68ef
Taylor McKinney <taylorsmck@gmail.com>
2024-02-23 18:32:11 +0900

    feat: updated glad linking

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(__attribute__((unused))GLFWwindow* window, int width, int height) {
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
 
 int main() {
     glfwInit();
@@ -7,7 +24,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Taylor's Attempt to Learn OpenGL", NULL, NULL);
 
     if (!window) {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -16,10 +33,28 @@ int main() {
     }
 
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        std::cout << "Failed to load GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glClearColor(0.4f, 0.3f, 0.4f, 0.0f);
 
+    // float vertices[] = {
+    //     -0.5f, -0.5f, 0.0f,
+    //      0.5f, -0.5f, 0.5f,
+    //      0.0f,  0.5f, 0.0f
+    // };
+
+    // unsigned int VBO;
+    // glGenBuffers(1, &VBO);
+
     while (!glfwWindowShouldClose(window)) {
-        // Render here
+        processInput(window);
+
+        // Render
 
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
