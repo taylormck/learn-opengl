a96a7340813d0015d8080e345eb1fc8dd3bc3c9d
Taylor McKinney <taylor.mckinney@revcomm.co.jp>
2024-02-25 01:47:57 +0900

    feat: made the rainbow triangle

--- main.cpp
+++ main.cpp
@@ -50,15 +50,16 @@ int main() {
     std::string shaderFolder = root.string() + "/../shaders/";
 
     Shader myShader(
-        shaderFolder + "vertex/identityOut.vert",
-        shaderFolder + "fragment/positionColor.frag"
+        shaderFolder + "vertex/identityColorOut.vert",
+        shaderFolder + "fragment/colorIn.frag"
     );
 
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        // vertices         // colors
+        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
     };
 
     unsigned int VBO, VAO;
@@ -68,8 +69,14 @@ int main() {
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
