e66928fc48c71ee9d902ae7db63cee7c2581aac3
Taylor McKinney <taylorsmck@gmail.com>
2024-03-17 03:04:50 +0900

    feat: separate emission shader

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <boost/format.hpp>
-
 #include "camera/Camera.hpp"
 #include "camera/FlyingCamera.hpp"
 #include "shader.hpp"
